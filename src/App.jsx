e161b11b28933bb60d8ac96e784d92499f540e84
Zoloo-zoloo <zoloozloo123@gmail.com>
2026-04-15 10:53:15 +0800

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import TopBar from './Components/Topbar'
+import TopBar from './Components/TopBar'
 import Intro from './Components/Intro'
 import ProgramList from './Components/ProgramList'
 import TrainerSection from './Components/TrainerSection'
 import BottomFooter from './Components/BottomFooter'
+
 const App = () => {
   return (
     <div className='bg-black'>
