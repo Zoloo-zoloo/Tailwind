0b6529d9dfbd00507e73aa2bf32002fe8f83e601
Zoloo-zoloo <zoloozloo123@gmail.com>
2026-04-22 07:57:51 +0800

    gap

--- src/Components/BottomFooter.jsx
+++ src/Components/BottomFooter.jsx
@@ -4,7 +4,7 @@ const BottomFooter = () => {
   return (
     <div className="flex flex-col items-center gap-3 md:flex-row md:justify-between md:items-center">
       <h1 className="text-lime-400 font-bold text-2xl">FitZone</h1>
-      <div className="items-end gap-10 text-gray-400 text-sm md:flex-row md:gap-10">
+      <div className="items-end gap-10 text-gray-400 text-sm md:flex-row ">
         <span >About</span>
         <span >Classes</span>
         <span >Membership</span>
