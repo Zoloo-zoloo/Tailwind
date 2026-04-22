9af040746b26129738a3ca2da914a5db42d148e5
Zoloo-zoloo <zoloozloo123@gmail.com>
2026-04-22 08:08:45 +0800

    my

--- src/Components/BottomFooter.jsx
+++ src/Components/BottomFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BottomFooter = () => {
   return (
-    <div className="flex flex-col items-center gap-3 md:flex-row md:justify-between md:items-center m-5">
+    <div className="flex flex-col items-center gap-3 md:flex-row md:justify-between md:items-center my-5">
       <h1 className="text-lime-400 font-bold text-2xl">FitZone</h1>
       <div className="flex items-end gap-5 text-gray-400 text-sm md:flex-row  ">
         <a href="#" className="text-white">About</a>
